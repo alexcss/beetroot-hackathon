ce2b20c756892dfaf6e0f77deb714c389e2cc8a1
AlexCSS <kalyuzhnyi@gmail.com>
2021-08-20 12:26:30 +0300

    add api

--- src/assets/js/_main.js
+++ src/assets/js/_main.js
@@ -12,5 +12,21 @@ export class Main {
     //define variables
 
     $(document).foundation();
+    this.getData();
+  }
+  getData(){
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer LXUMFibZNyO5ttA2qcGYvpT7uBZ6");
+
+    let requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=UA", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }
 }
