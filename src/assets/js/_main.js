e19405bcde97ce7a78278f6eb25cd6be89f8d731
AlexCSS <kalyuzhnyi@gmail.com>
2021-08-20 14:20:10 +0300

    add api url

--- src/assets/js/_main.js
+++ src/assets/js/_main.js
@@ -5,6 +5,12 @@
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 import './lib/foundation-explicit-pieces';
+// import './lib/jquery.vmap.min'
+// import './lib/maps/jquery.vmap.world'
+
+import 'jsvectormap'
+import 'jsvectormap/dist/maps/world'
+
 
 export class Main {
 
@@ -13,18 +19,66 @@ export class Main {
 
     $(document).foundation();
     this.getData();
+    this.initMap();
+  }
+  initMap(){
+    const map = new jsVectorMap({
+      selector: '#map',
+      map: 'world',
+      regionsSelectable: true,
+      showTooltip: true,
+      onRegionSelected: function (index, isSelected, selectedRegions) {
+        map.clearSelectedRegions();
+        console.log(index, isSelected, selectedRegions);
+        map.setSelected('regions', [index]);
+      },
+      onRegionTooltipShow: function (tooltip, code) {
+        tooltip.selector.innerHTML = tooltip.text() + ' <b>(Hello)</b>'
+      },
+      onLoaded(map) {
+        // This is a great opportunity and useful use-case to handle the reszing of the window.
+        window.addEventListener('resize', () => {
+          map.updateSize()
+        })
+      },
+      // -------- Series --------
+      series: {
+        // ---------- Region series ----------
+        regions: [
+          {
+            attribute: 'fill',
+            attributes: {
+              // EG: 'red'
+            },
+            legend: {
+              title: 'Country status',
+              vertical: true,
+            },
+            scale: {
+              "Free": "#4bdc77",
+              "Closed": "#ff5566"
+            },
+            values: {
+              GB: "Free",
+              MX: "Closed",
+              LY: "Free",
+            },
+          },
+        ]
+      },
+    })
   }
   getData(){
     let myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer LXUMFibZNyO5ttA2qcGYvpT7uBZ6");
+    myHeaders.append("Authorization", "Bearer JDiGZOvozuDN6G2G2k0HHYKiphyO5AQg");
 
     let requestOptions = {
+      origin: 'http://localhost:1234',
       method: 'GET',
       headers: myHeaders,
-      redirect: 'follow'
     };
 
-    fetch("https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=UA", requestOptions)
+    fetch("https://www.kayak.com/charm/horizon/uiapi/seo/marketing/travelrestrictions/CountriesTravelRestrictionsAction", requestOptions)
       .then(response => response.text())
       .then(result => console.log(result))
       .catch(error => console.log('error', error));
